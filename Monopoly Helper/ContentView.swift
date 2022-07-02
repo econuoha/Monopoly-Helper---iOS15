8ebecef8cfbc6d78223738b51dcde1d05ba767a8
Emmanuel Onuoha <emanonuoha@gmail.com>
2022-07-02 13:14:11 -0500

    Initial Commit

---
+++ Monopoly Helper/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Monopoly Helper
+//
+//  Created by Emmanuel Onouha on 7/2/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Monopoly Helper/Monopoly_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Monopoly_HelperApp.swift
+//  Monopoly Helper
+//
+//  Created by Emmanuel Onouha on 7/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Monopoly_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
